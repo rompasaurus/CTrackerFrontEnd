eb710d5d001ad41b98f10e943051838c2d8d6a30
rompasaurus <michael.e.mchenry@gmail.com>
2022-04-29 13:36:34 -0400

    style and grace

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,4 @@
 
-//export const BASE_URL = "http://192.168.0.154:8080"
-export const BASE_URL = "https://ridetally.com:8443"
+//export const BASE_URL = "http://192.168.1.153:8443"
+//export const BASE_URL = "https://ridetally.com:8443"
+export const BASE_URL = "http://192.168.1.56:8443"
